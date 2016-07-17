354c13c3a0d182c5e161c570d1d269a06ef1d198
Ronny Li <ronnyli@shijiebang.net>
2016-07-17 18:49:46 +0800

    change log config

---
+++ src/main/java/com/linkx/servlet/WeixinSignServlet.java
@@ -0,0 +1,24 @@
+package com.linkx.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author qings2329
+ *
+ * @since Jul 17, 2016   5:27:12 PM
+ */
+public class WeixinSignServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8149079703144644590L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+		
+	}
+	
+}
