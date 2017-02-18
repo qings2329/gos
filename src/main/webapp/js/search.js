81d818163a23e01477a13be99a0daa1e5d6c06ef
bitcoinreaver <qings2329@sina.com>
2017-02-19 01:39:25 +0800

    agent switch

---
+++ src/main/webapp/js/search.js
@@ -0,0 +1,45 @@
+/**
+ * Created by bitcoinreaver on 2/18/17.
+ */
+
+
+function agentModeSwitch() {
+    if($("#agent-cb")[0].checked) {
+        // 使用代理
+        $("a", [id=search]).each(function(){
+            var url = $(this).attr("href");
+            if(url.indexOf("/search?q=") == -1) {
+                var agentUrl = "/url?q=" + url + $(this).attr("param-data");
+                $(this).attr("href", agentUrl);
+            }
+        })
+    } else {
+        // 不使用代理
+        $("a", [id=search]).each(function(){
+            var url = $(this).attr("href");
+            if(url.indexOf("/url?q=") > -1) {
+                var pIndex = url.indexOf("&sa=")
+                var originalUrl = url.substring(7, pIndex);
+                $(this).attr("href", originalUrl);
+            }
+        })
+    }
+}
+
+
+$(function () {
+
+    $("a", [id=search]).each(function(){
+        var url = $(this).attr("href");
+        if(url.indexOf("/url?q=") > -1) {
+            var pIndex = url.indexOf("&sa=")
+            var originalUrl = url.substring(7, pIndex);
+            var params = url.substr(pIndex)
+            $(this).attr("href", originalUrl);
+            $(this).attr("param-data", params)
+        }
+    })
+
+    $("#agent-cb").click(agentModeSwitch);
+
+})
