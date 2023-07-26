0d19897cfd2d734600b95021e63f1f432518d727
limin <qings2329@sina.com>
2023-07-26 08:32:45 +0800

    bst

--- src/main/java/com/test/BST.java
+++ src/main/java/com/test/BST.java
@@ -1,8 +1,10 @@
 package com.test;
 
+/**
+ * BinSearchTree
+ */
 public class BST {
 
-    // BST(int), n,  => n=1, max, n=2, next to max
 
     public class Node {
         public Node left;
@@ -10,24 +12,24 @@ public class BST {
         public int value;
     }
 
-
+    // BST(int), n,  => n=1, max, n=2, next to max
     public Integer find(Node node, int n) {
-
-        if(node == null) {
+        if (node == null) {
             return null;
         }
 
         Integer right = find(node.right, n);
-        if(right != null) {
+        if (right != null) {
             return right;
         }
         n = n - 1;
-        if(n == 0) {
+        if (n == 0) {
             return node.value;
         }
         return find(node.left, n);
     }
 
+    // check whether a given tree is a bst
 
 
 }
